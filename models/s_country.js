31e54d69ba50d1dda03e3c0c5f88c34444c8f3b3
unknown <anubhav@vartulz.com>
2022-10-23 16:54:00 +0530

    general settings

---
+++ models/s_country.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CountrySchema = Schema({
+    name: {
+        type: String
+    },
+    iso: {
+        type: String
+    },
+    nickname: {
+        type: Number
+    },
+    status: {
+        type: Number,
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = { Country: mongoose.model('Country', CountrySchema) };
